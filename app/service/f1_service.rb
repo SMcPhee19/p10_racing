b07588e3d99b5ce5cdf6c38ad768fd04a065844f
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-07-30 23:52:14 -0600

    refactor: get rid of caching, not working, will come back later

--- app/service/f1_service.rb
+++ app/service/f1_service.rb
@@ -2,21 +2,15 @@
 
 class F1Service
   def get_drivers(driver_season)
-    Rails.cache.fetch([driver_season], expires: 1.week) do
       get_url("/api/f1/#{driver_season}/drivers.json")
-    end
   end
 
   def get_result(race_season, round)
-    Rails.cache.fetch([race_season, round], expires: 1.week) do
       get_url("/api/f1/#{race_season}/#{round}/results.json")
-    end
   end
 
   def get_constructors(constructors_season)
-    # Rails.cache.fetch([constructors_season], expires: 1.week) do
     get_url("/api/f1/#{constructors_season}/constructors.json")
-    # end
   end
 
   def get_schedule(season)
