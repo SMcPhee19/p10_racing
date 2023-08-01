39b01cffd0e3f3de5034d25b2ba69cbda0861053
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-08-01 01:10:23 -0600

    feat: sort the user/user_picks page by race from oldest to most recent

--- app/controllers/users/user_picks_controller.rb
+++ app/controllers/users/user_picks_controller.rb
@@ -5,6 +5,7 @@ module Users
     def index
       @user = User.find(params[:user_id])
       @races = F1Facade.new.get_schedule(@user.seasons.last.season_year)
+      @user_picks = @user.user_picks.order(created_at: :asc)
     end
   end
 end
