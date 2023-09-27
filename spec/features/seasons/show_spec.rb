5f2357451b9b1aaa6f5ed64c031b81f4265736b3
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-09-27 15:03:02 -0600

    test: view testing

--- spec/features/seasons/show_spec.rb
+++ spec/features/seasons/show_spec.rb
@@ -1,23 +1,26 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'timecop'
 
 RSpec.describe 'Season Home Page', vcr: { record: :new_episodes } do
   describe 'happy path' do
     before(:each) do
       @season = Season.create!(season_year: 2023)
-      @user1 = User.create!(name: 'Charles', total_points: 101)
-      @user2 = User.create!(name: 'Lewis', total_points: 134)
-      @user3 = User.create!(name: 'George', total_points: 161)
-      @user4 = User.create!(name: 'Lando', total_points: 187)
-      @user5 = User.create!(name: 'Alex', total_points: 110)
-      @user6 = User.create!(name: 'Yuki', total_points: 199)
+      @user1 = User.create!(name: 'Charles Leclerc', total_points: 101)
+      @user2 = User.create!(name: 'Lewis Hamilton', total_points: 134)
+      @user3 = User.create!(name: 'George Russell', total_points: 161)
+      @user4 = User.create!(name: 'Lando Norris', total_points: 187)
+      @user5 = User.create!(name: 'Alex Albon', total_points: 110)
+      @user6 = User.create!(name: 'Yuki Tsunoda', total_points: 199)
       UserSeason.create!(user_id: @user1.id, season_id: @season.id)
       UserSeason.create!(user_id: @user2.id, season_id: @season.id)
       UserSeason.create!(user_id: @user3.id, season_id: @season.id)
       UserSeason.create!(user_id: @user4.id, season_id: @season.id)
       UserSeason.create!(user_id: @user5.id, season_id: @season.id)
       UserSeason.create!(user_id: @user6.id, season_id: @season.id)
+      fixed_date = '2023-09-09'
+      Timecop.freeze(fixed_date)
       visit season_path(@season)
     end
 
@@ -54,10 +57,60 @@ RSpec.describe 'Season Home Page', vcr: { record: :new_episodes } do
       within '#page_title' do
         expect(page).to have_content('Season at a Glance')
       end
-      
+
       within '#season_year' do
         expect(page).to have_content("Season Year: #{@season[:season_year]}")
       end
     end
+
+    it 'page has buttons to all users show page and to new user' do
+      within '#all_users' do
+        User.all.each do |user|
+          expect(page).to have_button(user.name)
+          click_button user.name
+          expect(current_path).to eq(user_path(user.id))
+          visit season_path(@season)
+        end
+        expect(page).to have_link('New User')
+      end
+    end
+
+    it 'page has a drivers standings table' do
+      within '#driver_table' do
+        expect(page).to have_content('Driver Standings:')
+        expect(page).to have_css('table', text: 'Position')
+        expect(page).to have_css('table', text: 'Points')
+        expect(page).to have_css('table', text: 'Driver Name')
+        expect(page).to have_css('table', text: 'Constructor')
+      end
+    end
+
+    it 'page has a constructors standings table' do
+      within '#constructor_table' do
+        expect(page).to have_content('Constructor Standings:')
+        expect(page).to have_css('table', text: 'Position')
+        expect(page).to have_css('table', text: 'Constructor')
+        expect(page).to have_css('table', text: 'Wins')
+        expect(page).to have_css('table', text: 'Points')
+      end
+    end
+
+    it 'has a details for the next race - non-sprint weekend' do
+      fixed_date = '2023-09-09'
+      Timecop.freeze(fixed_date)
+
+      within '#next_race' do
+        expect(page).to have_content('Next Race: Singapore Grand Prix')
+        expect(page).to have_content('Circuit: Marina Bay Street Circuit')
+        expect(page).to have_content('Dates:')
+        expect(page).to have_content('Race: September 17: 06:00 AM')
+        expect(page).to have_content('Free Practice 1: September 15: 03:30 AM')
+        expect(page).to have_content('Free Practice 2: September 15: 07:00 AM')
+        expect(page).to have_content('Free Practice 3: September 16: 03:30 AM')
+        expect(page).to have_content('Qualifying: September 16: 07:00 AM')
+
+        Timecop.return
+      end
+    end 
   end
 end
