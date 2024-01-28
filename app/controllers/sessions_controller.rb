427e94fc1c987ae13dec9a8b68c0296543dadc8f
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2024-01-28 00:23:44 -0700

    chore: create the sessons controller for user login and logout
    Co-authored-by: mynamejoshg <mynamejoshg@users.noreply.github.com>

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,40 @@
+class SessionsController < ApplicationController
+  skip_before_action :authorize
+
+  def new
+    @hide_header = true
+  end
+
+  def create
+    @hide_header = true
+    @user = User.find_by(username: params[:username])
+    if (@user.validate_user_password(params[:password]))
+      # User is now authenticated
+      session[:username] = @user.username
+      # If there's a redirect specified, send there. Otherwise go to home page.
+      if (params[:redirect_url].present?)
+        require 'pry'; binding.pry
+        redirect_to params[:redirect_url], notice: "redirecting to #params[:redirect_url]"
+      else
+        redirect_to '/', notice:'Successfully returned to root path'
+      end
+    else
+      # Return generic error message and route nowhere
+      flash.now[:error] = 'Invalid email or password'
+      flash.now[:username] = params[:username]
+      render :new
+    end
+  end
+
+  def destroy
+     session.delete :username
+     redirect_to '/sessions/new', notice:'Redirecting to login page'
+  end
+
+  # def guest_login
+  #   Implement guest login logic
+  #   For example, you can create a temporary guest user
+  #   and log them in automatically
+  #   redirect_to appropriate path
+  # end
+end
