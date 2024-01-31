a4c050b3b3c630c180c6e1f7837be12d0b7f5469
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2024-01-30 23:38:31 -0700

    wip: json payloads for user claims
    Co-authored-by: mynamejoshg <mynamejoshg@users.noreply.github.com>

--- app/service/jwt_service.rb
+++ app/service/jwt_service.rb
@@ -5,61 +5,38 @@ require 'OpenSSL'
 class JwtService
   def create_token(user: User)
       pidalg = "HS256"
-      pidorg = "aaaaaaaa-a1b2-123a-b456-1234abcd5678"
-      pidtok = "c9fed74c5c994509b849ff65adb367d1"
       
       timestamp = Time.now.utc.strftime("%Y-%m-%d %H:%M:%S.000")
       pidkey = "JWC41crr322aUfdckVfJKHvGKNIPyAPGL7rMsTbzHlA="
+      hmac_secret = "imAs3cret!"
 
       #jwt header
       jwtheader = {
           "alg": pidalg,
-          "org_alias": pidorg,
-          "token": pidtok
+          "typ": "JWT"
       }
 
-      # claims = SELECT name FROM UserClaims uc INNER JOIN users_x_userclaims uxuc ON uc.user_id = uxuc.user_id WHERE uxuc.user_id = user.user_id
-      claims = UserClaim.joins("INNER JOIN users_x_userclaims ON users_x_userclaims.userclaim_id = userclaims.id").where(users_x_userclaims: { userclaim_id: user.id }).pluck(:name)
-      require 'pry'; binding.pry
-
-
       
+      claims = UserClaim.joins("INNER JOIN user_claims_users ON user_claims_users.user_claim_id = user_claims.id").where(user_claims_users: { user_claim_id: user.id }).pluck(:name)
+      require 'pry'; binding.pry
       
       #jwt payload
       jwtpayload = {
           "reqHeader":{
               "locale":"en",
-              "orgAlias":pidorg,
-              "secretKey":pidtok,
               "timestamp":timestamp,
-              "version":"4.6"
+              "version":"1.0"
           },
           "reqBody":{
               "userName":user.username,
               "userClaims":claims,
           }
       }
+      test_token = JWT.encode jwtpayload, hmac_secret, 'HS256'
+      puts test_token
+  end
 
-      jwtheaderJSON = jwtheader.to_json
-      jwtheaderUTF = jwtheaderJSON.encode("UTF-8")
-      tokenheader = Base64.urlsafe_encode64(jwtheaderUTF)
-      puts tokenheader
-
-      jwtpayloadJSON = jwtpayload.to_json
-      jwtpayloadUTF = jwtpayloadJSON.encode("UTF-8")
-      tokenpayload = Base64.urlsafe_encode64(jwtpayloadUTF)
-      puts tokenpayload
-
-      signeddata = tokenheader + "." + tokenpayload
-
-      digest = OpenSSL::Digest.new('sha256')
-      bin_key = Base64.decode64(pidkey)
-      instance = OpenSSL::HMAC.digest(digest, bin_key, signeddata)
-      signature = Base64.urlsafe_encode64(instance).chomp[0...-1]
-      puts signature
-
-      apitoken = signeddata + "." + signature
-
-      puts apitoken
+  def decode(token: String)
+    decoded_token = JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
   end
 end
