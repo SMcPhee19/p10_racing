0f98f9046abeeb47b27d2b3320ff6c13f3ae0400
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2024-02-08 16:40:57 -0700

    wip: reactive hamburger menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,24 @@
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
+// app/assets/javascripts/application.js
+document.addEventListener("DOMContentLoaded", function() {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+
+  hamburger.addEventListener("click", mobileMenu);
+
+  function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
+});
+
+const navLink = document.querySelector(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active")
+  navMenu.classList.remove("active")
+}
