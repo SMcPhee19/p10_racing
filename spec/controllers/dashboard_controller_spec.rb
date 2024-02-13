c1c44f12b27355e26f6903c5634e22f014049de8
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2024-02-13 16:53:42 -0700

    fix:  failing tests in dashboard controller

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -5,26 +5,9 @@ require 'rails_helper'
 RSpec.describe DashboardController, type: :controller, vcr: { record: :new_episodes } do
   before(:each) do
     @season = Season.create!(season_year: 2023)
-    @user = User.create!(name: 'Max Verstappen', username: 'MaxRacer33', pw_salt: '1a2b3c4d-5e6f-7g8h-9i10-j11k12l13m14',
+    @user = User.create!(name: 'Max Verstappen', username: 'MaxRacer33', pw_salt: '1a2b3c4d-5e6f-7g8h-9i10-j11k12l13m14', # rubocop:disable Layout/LineLength
                          pw_hash: '6f7ab8c9d10e11f12g13h14i15j16k17l18m19n20o21p22q23r24s25t26u27v28w29x30y31z32')
-    # visit '/'
-    # fill_in 'Username', with: 'MaxRacer33'
-    # fill_in 'Password', with: 'lew1sGotRobbed!1'
-    # click_button 'Login'
-    # save_and_open_page
-    password = 'lew1sGotRobbed!1'
-
-    # Get the new pw_salt and pw_hash
-    pw_salt = SecureRandom.uuid
-    prehash = @user.get_pw_salt_concat(pw_salt, password)
-    pw_hash = @user.calculate_hash(prehash)
-
-    # Now you can use these values to update the user's pw_salt and pw_hash
-    @user.update(pw_salt:, pw_hash:)
-
-    # Display the generated values
-    puts "New pw_salt: #{pw_salt}"
-    puts "New pw_hash: #{pw_hash}"
+    log_in_user(@user)
   end
 
   describe 'GET #index' do
