08c5df41074e453745abef21ce2fcd5d15bd9c09
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2024-02-13 16:53:07 -0700

    chore: code lines required to use authentication helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,9 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -68,6 +71,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include AuthenticationHelper, type: :controller
 end
 
 VCR.configure do |config|
