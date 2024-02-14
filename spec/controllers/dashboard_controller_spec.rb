8230fc835fd9477a5df08322bbf7e354d0b84698
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2024-02-13 17:04:05 -0700

    refactor: add rubocop exclusions

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe DashboardController, type: :controller, vcr: { record: :new_episodes } do
   before(:each) do
     @season = Season.create!(season_year: 2023)
-    @user = User.create!(name: 'Max Verstappen', username: 'MaxRacer33', pw_salt: '1a2b3c4d-5e6f-7g8h-9i10-j11k12l13m14', # rubocop:disable Layout/LineLength
+    @user = User.create!(name: 'Max Verstappen', username: 'MaxRacer33', pw_salt: '1a2b3c4d-5e6f-7g8h-9i10-j11k12l13m14', 
                          pw_hash: '6f7ab8c9d10e11f12g13h14i15j16k17l18m19n20o21p22q23r24s25t26u27v28w29x30y31z32')
     log_in_user(@user)
   end
